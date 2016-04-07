d703bc44828ad85e28fa65ab8f9ed92a3a4b5e80
IgorMed <igor.medvedyev@gmail.com>
2016-04-07 19:58:25 -0400

    Pause feature works!

--- src/model/Game.java
+++ src/model/Game.java
@@ -9,169 +9,166 @@ public class Game
 {
 	private GameField mGameField;
 	private ACTION_TYPE mAction;
-	
+
 	private final static int GAME_SPEED = 1000;
 	private final static int SCORE_BASE = 100;
 	private boolean isGamePlay;
-	
+
 	private ModelListener mListener;
-	
-	
+
 	private int mLevel;
 	private int mScore;
-	
 
 	public Game()
 	{
 		mGameField = new GameField();
-		mLevel =1;
+		mLevel = 1;
 		mScore = 0;
 	}
-	
-	public void setPlay (boolean state)
+
+	public void setPlay(boolean state)
 	{
 		isGamePlay = state;
 	}
-	
-	public void startGame() 
+
+	public void startGame()
 	{
 		mGameField.startMove();
-		
-		
+
 		do
 		{
 			fireStartTurnEvent();
-			
-				do
-				{
-					fireSuccessfullMoveEvent();
-					turnWait();
-					
-				}
-				while (mGameField.moveShapeDown());
-				
-				fireLinesDeletedEvent(mGameField.deleteLines());
-					
-				
-			
-		}
-		while (mGameField.nextMove());
-		
+
+			do
+			{
+				fireSuccessfullMoveEvent();
+				turnWait();
+
+			} while (mGameField.moveShapeDown());
+
+			fireLinesDeletedEvent(mGameField.deleteLines());
+
+		} while (mGameField.nextMove());
+
 		gameOver();
 	}
-	
-	public void onLeftKeyAction ()
+
+	public void onLeftKeyAction()
 	{
 		if (mGameField.moveShapeLeft())
-		  	fireUpdateInterface();
+			fireUpdateInterface();
 		else
 			notifyError();
-		
+
 	}
-	
-	public void onRightKeyAction ()
+
+	public void onRightKeyAction()
 	{
 		if (mGameField.moveShapeRight())
-		  	fireUpdateInterface();
+			fireUpdateInterface();
 		else
 			notifyError();
 	}
-	
+
 	public void onUpKeyAction()
 	{
 		if (mGameField.rotate())
-		  	fireUpdateInterface();
+			fireUpdateInterface();
 		else
 			notifyError();
 	}
-	
+
 	public void onDownKeyAction()
 	{
 		mGameField.drop();
 		fireUpdateInterface();
 		notifyFigureDropped();
 	}
-	
-	
+
 	private int calculateWaitTime()
 	{
-		return GAME_SPEED/mLevel;
+		return GAME_SPEED / mLevel;
 	}
-	
+
 	private void fireUpdateInterface()
 	{
-		
+
 	}
-	
-	private void fireLinesDeletedEvent(List <Integer> deletedLines)
+
+	private void fireLinesDeletedEvent(List<Integer> deletedLines)
 	{
-		
+
 		if (deletedLines != null)
 		{
-			mScore += deletedLines.size()*SCORE_BASE;
+			mScore += deletedLines.size() * SCORE_BASE;
 			// show animation for deleted lines
 			fireSuccessfullMoveEvent();
 		}
-		
-		
+
 	}
-	
+
 	private void notifyError()
 	{
 		Toolkit.getDefaultToolkit().beep();
 	}
-	
+
 	private void notifyFigureDropped()
 	{
 		Toolkit.getDefaultToolkit().beep();
 	}
-	
+
 	private void gameOver()
 	{
 		System.out.println("Game Over!");
 	}
-	
+
 	public void fireStartTurnEvent()
 	{
 		// update score if needed
 		// update nextShape
 	}
-	
+
 	public void fireSuccessfullMoveEvent()
 	{
-		//if(mListener!=null)
+		// if(mListener!=null)
 		{
 			String message = mGameField.boardToString();
-			//System.out.println("mlistener is not null");
+			// System.out.println("mlistener is not null");
 			mListener.onMove(message);// reprint board
-			//System.out.println(message);
+			// System.out.println(message);
 		}
 	}
-	
+
 	public void fireDeleteLinesEvent()
 	{
 		// animate line deletion
 	}
-	
-	public void setModelListener (ModelListener listener)
+
+	public void setModelListener(ModelListener listener)
 	{
 		mListener = listener;
 	}
-	private void turnWait ()
+
+	private void turnWait()
 	{
 		int timeFraction = 10;
-		int waitTime = calculateWaitTime()/timeFraction;
-		
-		for (int i = 0; i<timeFraction; i++)
+		int waitTime = calculateWaitTime() / timeFraction;
+
+		for (int i = 0; i < timeFraction; i++)
 		{
 			if (!isGamePlay)
-				try
+				while (!isGamePlay)
 				{
-					wait();
-				} catch (InterruptedException e)
-				{
-					// TODO Auto-generated catch block
-					e.printStackTrace();
+					try
+					{
+						Thread.sleep(100);
+					}
+
+					catch (InterruptedException e)
+					{
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
 				}
 			else
 				try
@@ -183,18 +180,12 @@ public class Game
 					e.printStackTrace();
 				}
 		}
-		
-	}
-	
-	
 
-	static enum ACTION_TYPE	{
-		ROTATE,
-		LEFT,
-		RIGHT,
-		DOWN,
-		DROP,
-		LINE_DELETE;
 	}
-	
+
+	static enum ACTION_TYPE
+	{
+		ROTATE, LEFT, RIGHT, DOWN, DROP, LINE_DELETE;
+	}
+
 }
